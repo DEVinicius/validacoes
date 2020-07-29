702cdaf76e23c35dd6df51e2d591e8d212d2c9b4
Vinicius Pereira de Oliveira <agavi2014@hotmail.com>
2020-07-29 11:57:03 -0300

    retirada de uma funcao obsoleta

--- Validation.php
+++ Validation.php
@@ -27,13 +27,14 @@ class Validation{
         }
         return true;  
     }
+    
     public function cepValidation(string $cep)
     {
         
         // retira espacos em branco
         $cep = trim($cep);
         // expressao regular para avaliar o cep
-        $avaliaCep = ereg("^[0-9]{5}-[0-9]{3}$", $cep);
+        $avaliaCep = preg_match("^[0-9]{5}-[0-9]{3}$", $cep);
         
         // verifica o resultado
         if(!$avaliaCep) {            
